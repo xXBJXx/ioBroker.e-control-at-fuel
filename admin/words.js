56bed80aed046b663dcce5c5d4d5fb7b672b7db3
Issi <alex.morosow1@gmail.com>
2020-05-04 11:54:55 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'e-control-at-fuel adapter settings': {
+		'en': 'Adapter settings for e-control-at-fuel',
+		'de': 'Adaptereinstellungen für e-control-at-fuel',
+		'ru': 'Настройки адаптера для e-control-at-fuel',
+		'pt': 'Configurações do adaptador para e-control-at-fuel',
+		'nl': 'Adapterinstellingen voor e-control-at-fuel',
+		'fr': "Paramètres d'adaptateur pour e-control-at-fuel",
+		'it': "Impostazioni dell'adattatore per e-control-at-fuel",
+		'es': 'Ajustes del adaptador para e-control-at-fuel',
+		'pl': 'Ustawienia adaptera dla e-control-at-fuel',
+		'zh-cn': 'e-control-at-fuel的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};

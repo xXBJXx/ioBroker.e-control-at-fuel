2a6e9446ab0fd5368482450a0443f6d7cf2e848c
Issi <alex.morosow1@gmail.com>
2020-05-20 21:29:56 +0200

    station PINK + logo added

--- main.js
+++ main.js
@@ -600,7 +600,7 @@ class EControlAtFuel extends utils.Adapter {
 						};
 
 						const logosName = ['aral', 'eni', 'shell', 'omv', 'avanti', 'bp', 'jet', 'turmöl', 'lagerhaus', 'avia', 'a1', 'diskont', 'iq', 'sb_tankstelle', 'land', 'thrainer',
-							'inn_tank', 'fillup', 'gutmann', 'holzknecht', 'diesel', 'genol_lagerhaustankstelle', 'mosonyi', 'lagerhaus_genol', 'pfluger'];
+							'inn_tank', 'fillup', 'gutmann', 'holzknecht', 'diesel', 'genol_lagerhaustankstelle', 'mosonyi', 'lagerhaus_genol', 'pfluger', 'pink'];
 
 						console.log(`prices: ${JSON.stringify(logosName)}`);
 						this.log.debug(`logosName : ${JSON.stringify(logosName)}`);
